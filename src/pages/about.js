8afcd5349550b74dfcb41c3d7efabdc3b8e15a35
Iakovos Vogiatzoglou <9253011+iakovosvo@users.noreply.github.com>
2021-01-17 16:25:24 +0100

    feat: Add siteMetadata

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import { Link } from 'gatsby';
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <Link to="/">Home</Link>
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
-export default function Home() {
-  return <div>Hello world!</div>;
+export default function Home({ data }) {
+  return (
+    <div>
+      <h1>{data.site.siteMetadata.title}</h1>
+      <div>
+        Hello world!
+      </div>
+    </div>
+  );
 }
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
